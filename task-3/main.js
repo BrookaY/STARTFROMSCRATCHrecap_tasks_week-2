48a327494ad78d2e289c1f5fd8c0170f58cca642
Mabrouka Yaqoob <mabroukayaqoob@outlook.com>
2022-05-13 18:07:06 +0100

    Task 3 works with click function

--- task-3/main.js
+++ task-3/main.js
@@ -1,9 +1,14 @@
 
 
+
+
 async function updateImg() {
     let response = await fetch("https://api.thecatapi.com/v1/images/search");
     let data = await response.json();
     let img = document.querySelector("img");
     img.src = data[0].url;
+    
 }
 updateImg();
+
+document.addEventListener("click", updateImg);
